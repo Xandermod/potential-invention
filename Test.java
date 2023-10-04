57af73df4b20cd3bec1a0a3f903f2a9e263a6a2d
Alexander Maschler <135417648+Xandermod@users.noreply.github.com>
2023-10-04 21:22:03 +0200

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println("Hello world");
+        Test person1 = new Test();
+        person1.name("Alex");
+        person1.age(26);
     }
 }
+class Test {
+String name;
+int age;
+
