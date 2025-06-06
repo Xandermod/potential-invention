dde0f9819be87fc8d1406c23a3822d98554b4236
Alexander Maschler <135417648+Xandermod@users.noreply.github.com>
2025-06-06 13:57:48 +0200

    Set up Maven build and add Hello World test

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+    private final PrintStream originalOut = System.out;
+    private ByteArrayOutputStream outContent;
+
+    @BeforeEach
+    public void setUpStreams() {
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testMainOutputsHelloWorld() {
+        Test.main(new String[]{});
+        assertEquals("Hello world" + System.lineSeparator(), outContent.toString());
+    }
+}
